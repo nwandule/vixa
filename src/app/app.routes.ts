cb603fbe69eee1ebce9fca81451ce8d98db35d51
nwandule <ripfumelon97@gmail.com>
2025-09-18 16:55:45 +0200

    Modify Visa online

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,12 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProductCardComponent } from './product-card/product-card.component';
-import { AboutProductComponent } from './about-product/about-product.component';
 import { MyOrdersComponent } from './my-orders-component/my-orders-component.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: LandingPageComponent
-  },
-  {
-    path: 'product/:id',
-    component: AboutProductComponent
   },
   { path: 'my-orders', 
     component: MyOrdersComponent 
